d0a247078ad5e7e172edf9f7f7453cd3b69ce1dc
Sandro Spengler <Sandro.Spengler@web.de>
2021-11-29 23:59:17 +0100

    MongoDB Connection working

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,27 @@
+const mongoose = require("mongoose");
+
+const { DB_CONNECTION, PORT } = require("./Config/config");
+
 const express = require("express");
 
 const APP = express();
-const PORT = 5000;
 
 APP.get("/", (req: any, res: any) => {
   res.send("<h1>Main Page!!!!</h1>");
 });
 
 APP.use("/api/character", require("./Route/Api/CharacterController"));
+
+const connectToMongoDB = (): void => {
+  mongoose
+    .connect(DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err: Error) => {
+      console.log(err.message);
+    });
+};
+
+connectToMongoDB();
 
 APP.listen(PORT, () => {
   console.log("Server is running");
